62fbb941a0d5aa57fcf210f7deb01b95a897cdf9
Danila Gruntov <2007gruntov@gmail.com>
2026-01-13 16:42:30 +0300

    Сделал главное окно и добавил страницы регистрации и входа

--- Parfuholic/MainWindow.xaml.cs
+++ Parfuholic/MainWindow.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfApp;
 
 namespace Parfuholic
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -30,6 +15,12 @@ namespace Parfuholic
 
             // Проверяем подключение
             dbHelper.TestConnection();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Навигация на страницу входа
+            LoginPage.Navigate(new LoginPage());
         }
     }
 }
